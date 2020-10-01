d5b51fda5c1351a34ca5ea29eefa25fc0978984c
Mark Wylde <me@markwylde.com>
2020-10-01 22:56:38 +1000

    Add changes for the puzed-installer and docker

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 module.exports = {
   oauthClientId: process.env.OAUTH_CLIENT_ID || 'Iv1.6e1c81a97956c1d1',
-  apiServerUrl: 'https://api.puzed.test'
+  apiServerUrl: process.env.API_SERVER_URL || 'https://api.puzed.test'
 };
