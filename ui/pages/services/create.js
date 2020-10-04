a71c03cc0f6c96ca282477022ef428bfd345623a
Mark Wylde <me@markwylde.com>
2020-10-04 14:11:44 +1000

    List errors on validation

--- ui/pages/services/create.js
+++ ui/pages/services/create.js
@@ -24,6 +24,11 @@ function createForm ({ attrs }) {
           <div class="alert alert-danger">
             <div><strong>Could not create service</strong></div>
             <div>Check and fix any specific errors in the form below then try again.</div>
+            ${errors.messages ? html`
+              <ul>
+                ${errors.messages.map(message => html`<li>${message}</li>`)}
+              </ul>
+            ` : ''}
           </div>
         `),
 
